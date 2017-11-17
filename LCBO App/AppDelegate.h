2b51cd9c74e7539550be537fbe3c78b855d38264
Paul <paul.huynh3@gmail.com>
2017-11-17 17:05:26 -0500

    initial

---
+++ LCBO App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCBO App
+//
+//  Created by Paul on 2017-11-17.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
