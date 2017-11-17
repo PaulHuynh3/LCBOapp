d843940782cc311565a559138b83f3aa23b0b58a
Paul <paul.huynh3@gmail.com>
2017-11-17 17:13:25 -0500

    create alcohol model

---
+++ LCBO App/Model/Alcohol.h
@@ -0,0 +1,13 @@
+//
+//  Alcohol.h
+//  LCBO App
+//
+//  Created by Paul on 2017-11-17.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Alcohol : NSObject
+
+@end
---
+++ LCBO App/Model/Alcohol.m
@@ -0,0 +1,13 @@
+//
+//  Alcohol.m
+//  LCBO App
+//
+//  Created by Paul on 2017-11-17.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import "Alcohol.h"
+
+@implementation Alcohol
+
+@end
